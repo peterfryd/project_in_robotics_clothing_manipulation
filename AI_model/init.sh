f4a38e0896d82177c453b6074018a5b3a854c47c
Anders Lind-Thomsen <anders@lind-thomsen.dk>
2025-09-17 12:27:44 +0200

    working openvla

--- AI_model/init.sh
+++
@@ -1,6 +0,0 @@
-pip install transformers datasets evaluate accelera
-pip install typeguard torchvision pillow cython opencv-python
-pip install ipywidgets
-pip install -U "huggingface_hub[cli]"
-pip install torch
-pip install -U transformers datasets evaluate accelerate timm
---
+++ AI_model/openvla_mini init.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+pip install -r https://raw.githubusercontent.com/openvla/openvla/main/requirements-min.txt
+pip install accelerate
