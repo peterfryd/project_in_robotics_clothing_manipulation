61f40a52d487b290a6e103b0e15bf8723fcfde81
peterfryd <frydensberg.peter@gmail.com>
2025-09-24 11:18:35 +0200

    The run_programme main should work now

--- src/system_integrator/system_integrator/run_programme.py
+++ src/system_integrator/system_integrator/run_programme.py
@@ -1,60 +1,99 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+import threading
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
 from system_integrator.srv import Programme
 from vla_inference.srv import ServiceSendToModel
-
+from robot_controller_pkg.srv import RobotCmd
 
 class RunProgramme(Node):
-    def __init__(self):
+    def __init__(self,):
         super().__init__('run_programme')
-        self.bridge = CvBridge()
         self.latest_image = None
+        self.image_lock = threading.Lock()
 
         # Subscribe to camera
-        self.create_subscription(Image, '/camera/color/image_raw', self.image_callback, 10)
+        self.create_subscription(Image, '/camera/camera/color/image_raw', self.image_callback, 10)
 
         # Create service
-        self.srv = self.create_service(Programme, 'run_programme', self.run_programme)
+        self.srv = self.create_service(Programme, 'service_run_programme', self.run_programme)
         
-        # Create client for the other service
-        self.other_service_client = self.create_client(ServiceSendToModel, 'INSERT NAME HERE')
+        # Create client for service_send_to_model
+        self.ai_service = self.create_client(ServiceSendToModel, 'service_send_to_model')
+        
+        # Create client for service_send_to_model
+        self.robot_service = self.create_client(RobotCmd, 'robot_cmd_service')
 
         # Wait until the other service is available
-        while not self.other_service_client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('Waiting for /move_command service...')
+        while not self.ai_service.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Waiting for service_send_to_model...')
+            
+             # Wait until the other service is available
+        while not self.robot_service.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Waiting for robot_cmd_service...')
 
     def image_callback(self, msg):
-        self.latest_image = msg  # Store as ROS Image
-
+        with self.image_lock:
+            self.latest_image = msg  # Store as ROS Image
+    
     def run_programme(self, request, response):
-        if self.latest_image is None:
-            self.get_logger().warn("No image received yet.")
-            response.success = False
+        img = None
+        with self.image_lock:
+            if self.latest_image is None:
+                self.get_logger().warn("No image received yet.")
+                response.success = False
+                return response
+            else: 
+                img = self.latest_image
         
         
-         # --- CALL THE OTHER SERVICE ---
-        other_request = MoveCommand.Request()
-        other_request.prompt = request.prompt
-        other_request.image = self.latest_image
+        #  # --- CALL THE AI SERVICE ---
+        ai_request = ServiceSendToModel.Request()
+        ai_request.prompt = request.prompt
+        
+        ai_request.image = img
 
         # Call synchronously (blocking) — you could also do async
-        future = self.other_service_client.call_async(other_request)
+        future = self.ai_service.call_async(ai_request)
         rclpy.spin_until_future_complete(self, future)
+        
+        delta_position = None
+        delta_orientation = None
+        delta_gripper = None
+        
         if future.result() is not None:
             result = future.result()
-            response.delta_position = result.delta_position
-            response.delta_gripper = result.delta_gripper
-            response.success = True
-            self.get_logger().info(f"Received response from /move_command: {result}")
+            delta_position = result.delta_position
+            delta_orientation = result.delta_orientation
+            delta_gripper = result.delta_gripper
+            self.get_logger().info(f"Received response from service_send_to_model: {result}")
         else:
-            self.get_logger().error("Failed to call /move_command")
+            self.get_logger().error("Failed to call service_send_to_model")
             response.success = False
+            return response
         
-        response.success = True 
         
+        #  # --- CALL THE ROBOT SERVICE ---
+        robot_request = RobotCmd.Request()
+        
+        robot_request.delta_position = delta_position
+        robot_request.delta_orientation = delta_orientation
+        robot_request.delta_gripper = delta_gripper
+
+        # Call synchronously (blocking) — you could also do async
+        future = self.robot_service.call_async(robot_request)
+        rclpy.spin_until_future_complete(self, future)
+        
+        if future.result() is not None:
+            result = future.result()
+            response.success = result.success
+            self.get_logger().info(f"Received response from robot_cmd_service: {result}")
+        else:
+            self.get_logger().error("Failed to call robot_cmd_service")
+            response.success = False
+            return response        
+    
         return response
 
 
