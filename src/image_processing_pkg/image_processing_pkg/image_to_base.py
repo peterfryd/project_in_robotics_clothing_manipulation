9e02cf684c71dcf0aa4a1bdb64547b1a5575dbab
alexellegaard <alexellegaard@gmail.com>
2025-10-29 09:58:36 +0100

    updated image_to_base node

--- src/image_processing_pkg/image_processing_pkg/image_to_base.py
+++ src/image_processing_pkg/image_processing_pkg/image_to_base.py
@@ -9,24 +9,30 @@ class ImageToBaseNode(Node):
     def __init__(self):
         super().__init__('image_to_base_node')
 
-        # Camera height to table
-        self.camera_height = 1.0
-
         # Camera intrinsics
         self.fx = 891.01611328125
         self.fy = 891.01611328125
         self.cx = 642.7546997070312
         self.cy = 367.76971435546875
         
-        # tcp to base transform
-        self.R_tcp_to_base = self.rotation_matrix_from_euler_xyz(-np.pi/2, 0.0, 1.992)
-        self.t_tcp_to_base = np.array([0.473, 0.230, -0.530])
+        # tcp to cam (mounting offset)
+        self.R_tcp_to_cam = np.array([[0,1,0],
+                                      [0,0,-1],
+                                      [-1,0,0]])
+        self.t_tcp_to_cam = np.array([-0.045, -0.06, -0.01])
+
+        # base to tcp 
+        self.R_base_to_tcp = self.axis_angle_to_rotation_matrix(0.973, -1.514, -1.506)
+        self.t_base_to_tcp = np.array([-0.473, -0.230, 0.930])
         # joint pose = [33.37, -77.23, 37.00, 220.04, -147.22, -180.21]
         
-        # cam to tcp transform (mounting offset)
-        self.R_cam_to_tcp = self.rotation_matrix_from_euler_xyz(-np.pi/2, 0.0, 0.0)
-        self.t_cam_to_tcp = np.array([0.045, 0.06, 0.01])
 
+        # Camera height to table
+        self.camera_height = 0.928 - 0.06 + 0.035
+        self.R_base_to_cam = self.R_base_to_tcp @ self.R_tcp_to_cam
+        self.t_base_to_cam = self.R_base_to_tcp @ self.t_tcp_to_cam + self.t_base_to_tcp
+        self.camera_height = self.t_base_to_cam[2] + 0.05
+        
         # Service
         self.srv = self.create_service(
             ImageToBase,
@@ -40,36 +46,45 @@ class ImageToBaseNode(Node):
         u, v = request.imageframe_coordinates
 
         Z_cam = self.camera_height
-        Z_cam = 2.047
-        u, v = 560, 350
+        #u, v = 640, 360
+
         X_cam = (u - self.cx) * Z_cam / self.fx
         Y_cam = (v - self.cy) * Z_cam / self.fy
 
         P_cam = np.array([X_cam, Y_cam, Z_cam])
+
+        self.get_logger().info(f"Projected ({u}, {v}) to P_cam: [{P_cam[0]:.3f}, {P_cam[1]:.3f}, {P_cam[2]:.3f}]")
         
         # Transformation from camera to TCP
-        P_tcp = self.R_cam_to_tcp @ P_cam + self.t_cam_to_tcp
+        P_tcp = self.R_tcp_to_cam @ P_cam + self.t_tcp_to_cam
 
         # Transformation from TCP to base
-        P_base = self.R_tcp_to_base @ P_tcp + self.t_tcp_to_base
+        P_base = self.R_base_to_tcp @ P_tcp + self.t_base_to_tcp
 
         response.baseframe_coordinates = P_base.tolist()
 
-        self.get_logger().info(f"Projected ({u}, {v}) -> [{P_base[0]:.3f}, {P_base[1]:.3f}, {P_base[2]:.3f}]")
+        self.get_logger().info(f"Transformed ({u}, {v}) to P_base: [{P_base[0]:.3f}, {P_base[1]:.3f}, {P_base[2]:.3f}]")
 
         return response
+    
+    def axis_angle_to_rotation_matrix(self, r1, r2, r3):
+        r = np.array([r1, r2, r3])
+        angle = np.linalg.norm(r)
+        if angle < 1e-8:  # near zero angle
+            return np.eye(3)
 
-    def rotation_matrix_from_euler_xyz(self, roll, pitch, yaw):
-        Rx = np.array([[1, 0, 0],
-                    [0, np.cos(roll), -np.sin(roll)],
-                    [0, np.sin(roll), np.cos(roll)]])
-        Ry = np.array([[np.cos(pitch), 0, np.sin(pitch)],
-                    [0, 1, 0],
-                    [-np.sin(pitch), 0, np.cos(pitch)]])
-        Rz = np.array([[np.cos(yaw), -np.sin(yaw), 0],
-                    [np.sin(yaw), np.cos(yaw), 0],
-                    [0, 0, 1]])
-        return Rz @ Ry @ Rx
+        axis = r / angle
+        x, y, z = axis
+        c = np.cos(angle)
+        s = np.sin(angle)
+        t = 1 - c
+
+        R = np.array([
+            [t*x*x + c,     t*x*y - s*z, t*x*z + s*y],
+            [t*x*y + s*z,   t*y*y + c,   t*y*z - s*x],
+            [t*x*z - s*y,   t*y*z + s*x, t*z*z + c]
+        ])
+        return R
 
 
 def main(args=None):
