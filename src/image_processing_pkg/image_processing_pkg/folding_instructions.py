4b7d0494b8477f1d67c26af66100e6a2bcb0a9cb
peterfryd <frydensberg.peter@gmail.com>
2025-10-29 10:25:31 +0100

    folding instructions

--- src/image_processing_pkg/image_processing_pkg/folding_instructions.py
+++ src/image_processing_pkg/image_processing_pkg/folding_instructions.py
@@ -1,4 +1,6 @@
 
+from cmath import rect
+from math import inf
 import os
 import cv2
 from ament_index_python.packages import get_package_share_directory
@@ -46,10 +48,10 @@ def segment_foreground(image, background):
                     min_dist = dist
                     central_contour = cnt
 
-    forergound_mask = np.zeros(image.shape[:2], dtype=np.uint8)
-    cv2.drawContours(forergound_mask, [central_contour], -1, 255, -1)
+    foregroundd_mask = np.zeros(image.shape[:2], dtype=np.uint8)
+    cv2.drawContours(foregroundd_mask, [central_contour], -1, 255, -1)
     
-    return forergound_mask, central_contour
+    return foregroundd_mask, central_contour
 
 
 def load_background_image(image_name='background.png'):
@@ -67,13 +69,11 @@ def load_background_image(image_name='background.png'):
 
 
 def step_1_instructions(cv_image, background):
-    forergound_mask, central_contour = segment_foreground(cv_image, background)
+    foregroundd_mask, central_contour = segment_foreground(cv_image, background)
     
-    hull_points = cv2.convexHull(central_contour)  # Nx1x2
+    hull_points = cv2.convexHull(central_contour)
     hull_points = hull_points.reshape(-1, 2)
 
-    # --- Approximate hull to hexagon ---
-    # Use iterative approxPolyDP to reduce to 6 points
     epsilon = 0.01 * cv2.arcLength(hull_points, True)
     for i in range(100):
         approx = cv2.approxPolyDP(hull_points, epsilon, True)
@@ -81,18 +81,16 @@ def step_1_instructions(cv_image, background):
             break
         epsilon *= 1.05
 
-    # If we got fewer than 6 points, resample evenly
     if len(approx) < 4:
         approx = approx[np.linspace(0, len(approx)-1, 6, dtype=int)]
 
-    hexagon = approx.reshape(-1, 2)
+    rectangle = approx.reshape(-1, 2)
 
-    # Draw hexagon on image
     image_with_hull = cv_image.copy()
     distances = []
     for i in range(4):
-        pt1 = tuple(hexagon[i])
-        pt2 = tuple(hexagon[(i + 1) % 4])
+        pt1 = tuple(rectangle[i])
+        pt2 = tuple(rectangle[(i + 1) % 4])
         distances.append(float(np.linalg.norm(np.array(pt1) - np.array(pt2))))
         cv2.line(image_with_hull, pt1, pt2, (0, 0, 255), 2)
 
@@ -100,8 +98,8 @@ def step_1_instructions(cv_image, background):
 
     indices = np.argsort(np.array(distances))[-3:]
     for idx in indices:
-        pt1 = tuple(hexagon[idx])
-        pt2 = tuple(hexagon[(idx + 1) % 4])
+        pt1 = tuple(rectangle[idx])
+        pt2 = tuple(rectangle[(idx + 1) % 4])
         if pt1 in point_occurences:
             point_occurences[pt1] += 1
         else:
@@ -122,7 +120,7 @@ def step_1_instructions(cv_image, background):
         cx = int(M["m10"] / M["m00"])
         cy = int(M["m01"] / M["m00"])
     else:
-        cx, cy = 0, 0  # Avoid division by zero for degenerate contours
+        cx, cy = 0, 0
 
     cv2.circle(image_with_hull, (cx, cy), 5, (255, 0, 0), -1)
 
@@ -139,51 +137,233 @@ def step_1_instructions(cv_image, background):
         cv2.circle(image_with_hull, place_point, 5, (0, 0, 255), -1)
         
     #cv2.imwrite('/home/peter/uni/project_clothing_fresh/image_analysis/images/image_1_instructions.png', image_with_hull)
-    #print(pick_point, place_point)
     
     return pick_point, place_point
     
 
-def step_2_instructions(cv_image, background):
-    forergound_mask, central_contour = segment_foreground(cv_image, background)
+def step_2_instructions(cv_image, background, step_1_place):
+    foreground_mask, central_contour = segment_foreground(cv_image, background)
     
+    hull_points = cv2.convexHull(central_contour)
+    hull_points = hull_points.reshape(-1, 2)
+
+    epsilon = 0.01 * cv2.arcLength(hull_points, True)
+    for i in range(100):
+        approx = cv2.approxPolyDP(hull_points, epsilon, True)
+        if len(approx) <= 4:
+            break
+        epsilon *= 1.05
+
+    if len(approx) < 4:
+        approx = approx[np.linspace(0, len(approx)-1, 6, dtype=int)]
+
+    rectangle = approx.reshape(-1, 2)
+
+    image_with_hull = cv_image.copy()
+    
+    min_dist = inf
     pick_point = [0, 0]
-    place_point = [0, 0]
+    
+    for pt in rectangle:
+        dist = np.linalg.norm(pt - step_1_place)
+        if dist < min_dist:
+            min_dist = dist
+            pick_point = pt
+    
+    remaining_pts = np.array([pt for pt in rectangle if not np.allclose(pt, pick_point)])
+    max_dist = -inf
+    
+    for i in range(len(remaining_pts)):
+        for j in range(i + 1, len(remaining_pts)):
+            dist = np.linalg.norm(remaining_pts[i] - remaining_pts[j])
+            if dist > max_dist:
+                max_dist = dist
+                p1, p2 = remaining_pts[i], remaining_pts[j]
+
+    p1, p2 = np.array(p1, dtype=float), np.array(p2, dtype=float)
+
+    v = p2 - p1
+    place_point = np.astype(p1 + (p2-p1) / 2, np.int32).tolist()
+    
+    cv2.circle(image_with_hull, place_point, 5, (0, 0, 255), -1)
+    cv2.circle(image_with_hull, pick_point, 5, (255, 0, 0), -1)
+        
+    #cv2.imwrite('/home/peter/uni/project_clothing_fresh/image_analysis/images/image_2_instructions.png', image_with_hull)
+    
     return pick_point, place_point
 
 def step_3_instructions(cv_image, background):
-    forergound_mask, central_contour = segment_foreground(cv_image, background)
+    foreground_mask, central_contour = segment_foreground(cv_image, background)
     
-    pick_point = [0, 0]
-    place_point = [0, 0]
+    hull_points = cv2.convexHull(central_contour)
+    hull_points = hull_points.reshape(-1, 2)
+
+    epsilon = 0.01 * cv2.arcLength(hull_points, True)
+    for i in range(100):
+        approx = cv2.approxPolyDP(hull_points, epsilon, True)
+        if len(approx) <= 3:
+            break
+        epsilon *= 1.05
+
+    if len(approx) < 3:
+        approx = approx[np.linspace(0, len(approx)-1, 6, dtype=int)]
+
+    triangle = approx.reshape(-1, 2)
+
+    image_with_hull = cv_image.copy()
+    min_dist = inf
+    pt1 = [0,0]
+    pt2 = [0,0]
+    
+    for i in range(len(triangle)):
+        for j in range(i + 1, len(triangle)):
+            dist = np.linalg.norm(triangle[i] - triangle[j])
+            if dist < min_dist:
+                min_dist = dist
+                p1, p2 = triangle[i], triangle[j]
+    
+    pick_point = p2
+    place_point = np.astype(p2 + 0.5 * (p1 - p2), np.int32)
+
+    cv2.circle(image_with_hull, place_point, 5, (0, 0, 255), -1)
+    cv2.circle(image_with_hull, pick_point, 5, (255, 0, 0), -1)
+        
+   # cv2.imwrite('/home/peter/uni/project_clothing_fresh/image_analysis/images/image_3_instructions.png', image_with_hull)
+    
     return pick_point, place_point
 
-def step_4_instructions(cv_image, background):
-    forergound_mask, central_contour = segment_foreground(cv_image, background)
+def step_4_instructions(cv_image, background, step_3_pick, step_3_place):
+    foreground_mask, central_contour = segment_foreground(cv_image, background)
+    
+    hull_points = cv2.convexHull(central_contour)
+    hull_points = hull_points.reshape(-1, 2)
+
+    epsilon = 0.01 * cv2.arcLength(hull_points, True)
+    for i in range(100):
+        approx = cv2.approxPolyDP(hull_points, epsilon, True)
+        if len(approx) <= 4:
+            break
+        epsilon *= 1.05
+
+    if len(approx) < 4:
+        approx = approx[np.linspace(0, len(approx)-1, 6, dtype=int)]
+
+    rectangle = approx.reshape(-1, 2)
+
+    image_with_hull = cv_image.copy()
+    
+    place_point = [0, 0]
+    min_dist_pick = inf
+    
+    for pt in rectangle:
+        dist_pick = np.linalg.norm(pt - step_3_pick)
+        if dist_pick < min_dist_pick:
+            min_dist_pick = dist_pick
+            place_point = pt.tolist()
     
     pick_point = [0, 0]
-    place_point = [0, 0]
+    
+    remaining_pts = np.array([pt for pt in rectangle if not np.allclose(pt, place_point)])
+    
+    pick_point = [0, 0]
+    min_dist_place = inf
+    
+    for pt in remaining_pts:
+        dist_place = np.linalg.norm(pt - step_3_place)
+        if dist_pick < min_dist_place:
+            min_dist_place = dist_place
+            pick_point = pt.tolist()
+    
+    cv2.circle(image_with_hull, place_point, 5, (0, 0, 255), -1)
+    cv2.circle(image_with_hull, pick_point, 5, (255, 0, 0), -1)
+        
+    #cv2.imwrite('/home/peter/uni/project_clothing_fresh/image_analysis/images/image_4_instructions.png', image_with_hull)
     return pick_point, place_point
 
-def step_5_instructions(cv_image, background):
-    forergound_mask, central_contour = segment_foreground(cv_image, background)
+def step_5_instructions(cv_image, background, place_point_4):
+    foregroundd_mask, central_contour = segment_foreground(cv_image, background)
     
-    pick_point = [0, 0]
-    place_point = [0, 0]
-    return pick_point, place_point
+    hull_points = cv2.convexHull(central_contour)
+    hull_points = hull_points.reshape(-1, 2)
 
-def step_6_instructions(cv_image, background):
-    forergound_mask, central_contour = segment_foreground(cv_image, background)
+    epsilon = 0.01 * cv2.arcLength(hull_points, True)
+    for i in range(100):
+        approx = cv2.approxPolyDP(hull_points, epsilon, True)
+        if len(approx) <= 4:
+            break
+        epsilon *= 1.05
+
+    if len(approx) < 4:
+        approx = approx[np.linspace(0, len(approx)-1, 6, dtype=int)]
+
+    rectangle = approx.reshape(-1, 2)
+
+    image_with_hull = cv_image.copy()
+    
+    min_dist_pick = inf
+    
+    pt1 = None
+    pt2 = None
+    
+    for pt in rectangle:
+        cv2.circle(image_with_hull, pt, 5, (0, 255, 255), -1)
+        dist_pick = np.linalg.norm(pt - place_point_4)
+        if dist_pick < min_dist_pick:
+            min_dist_pick = dist_pick
+            pt1 = pt
+    
+    
+    remaining_pts = np.array([pt for pt in rectangle if not np.allclose(pt, pt1)])
     
     pick_point = [0, 0]
-    place_point = [0, 0]
+    min_dist_place = inf
+    
+    for pt in remaining_pts:
+        dist_place = np.linalg.norm(pt - pt1)
+        if dist_pick < min_dist_place:
+            min_dist_place = dist_place
+            pt2 = pt
+    
+    mid_point_pick = (pt2 + pt1) / 2
+    
+    rectangle_mid = np.mean(rectangle, axis=0)
+    
+    pick_point =(mid_point_pick + 50 * (rectangle_mid - mid_point_pick)/(np.linalg.norm(rectangle_mid - mid_point_pick)))
+    
+    
+    last_two_points  = np.array([pt for pt in remaining_pts if not np.allclose(pt, pt2)])
+    
+    place_point = np.mean(last_two_points, axis = 0)
+    
+    
+    rectangle_mid = np.astype(rectangle_mid, np.int32)
+    pick_point = np.astype(pick_point, np.int32)
+    place_point = np.astype(place_point, np.int32)
+    
+    cv2.circle(image_with_hull, rectangle_mid.tolist(), 5, (0, 0, 255), -1)
+    cv2.circle(image_with_hull, place_point, 5, (0, 0, 255), -1)
+    cv2.circle(image_with_hull, pick_point, 5, (255, 0, 0), -1)
+        
+    #cv2.imwrite('/home/peter/uni/project_clothing_fresh/image_analysis/images/image_5_instructions.png', image_with_hull)
     return pick_point, place_point
+    
+
 
 
 def main():
-    cv_image = cv2.imread("/home/peter/uni/project_clothing_fresh/image_analysis/images/1.png")
+    cv_image_1 = cv2.imread("/home/peter/uni/project_clothing_fresh/image_analysis/images/1.png")
+    cv_image_2 = cv2.imread("/home/peter/uni/project_clothing_fresh/image_analysis/images/2.png")
+    cv_image_3 = cv2.imread("/home/peter/uni/project_clothing_fresh/image_analysis/images/3.png")
+    cv_image_4 = cv2.imread("/home/peter/uni/project_clothing_fresh/image_analysis/images/4.png")
+    cv_image_5 = cv2.imread("/home/peter/uni/project_clothing_fresh/image_analysis/images/5.png")
+    
     back_ground = cv2.imread("/home/peter/uni/project_clothing_fresh/image_analysis/images/background.png")
-    pick_point, place_point = step_1_instructions(cv_image, back_ground)
+    
+    pick_point_1, place_point_1 = step_1_instructions(cv_image_1, back_ground)
+    pick_point_2, place_point_2 = step_2_instructions(cv_image_2, back_ground, pick_point_1)
+    pick_point_3, place_point_3 = step_3_instructions(cv_image_3, back_ground)
+    pick_point_4, place_point_4 = step_4_instructions(cv_image_4, back_ground, pick_point_3, place_point_3)
+    pick_point_5, place_point_5 = step_5_instructions(cv_image_5, back_ground, place_point_4)
     
 if __name__ == "__main__":
     main()
--- src/image_processing_pkg/image_processing_pkg/get_pick_and_place_point.py
+++ src/image_processing_pkg/image_processing_pkg/get_pick_and_place_point.py
@@ -4,21 +4,25 @@ import rclpy
 from rclpy.node import Node
 import numpy as np
 from custom_interfaces_pkg.srv import GetPickAndPlacePoint
-from image_processing_pkg.folding_instructions import load_background_image, step_1_instructions, step_2_instructions, step_3_instructions, step_4_instructions, step_5_instructions, step_6_instructions
+from image_processing_pkg.folding_instructions import load_background_image, step_1_instructions, step_2_instructions, step_3_instructions, step_4_instructions, step_5_instructions
 from sensor_msgs.msg import Image
 import threading
 from cv_bridge import CvBridge
-
 
 class GetPickAndPlacePointNode(Node):
     def __init__(self):
         super().__init__('get_pick_and_place_point_node')
         
+        self.place_point1 = None
+        self.place_point3 = None
+        self.pick_point3 = None
+        self.place_point4 = None
+        
         self.lock = threading.Lock()
         self.image = None
         self.bridge = CvBridge()
         
-        self.background = load_background_image()
+        self.background = load_background_image(image_name = 'background.png')
         
         self.sub = self.create_subscription(
             Image,
@@ -52,26 +56,42 @@ class GetPickAndPlacePointNode(Node):
                 return response
             
             cv_image = self.bridge.imgmsg_to_cv2(self.image, desired_encoding='bgr8')
-
-        step_functions = {
-            1: step_1_instructions,
-            2: step_2_instructions,
-            3: step_3_instructions,
-            4: step_4_instructions,
-            5: step_5_instructions,
-            6: step_6_instructions,
-        }
         
-        func = step_functions.get(step_number)
-        if func is not None:
-            image_pick_point, image_place_point = func(cv_image, self.background)
+        pick_point = [0, 0]
+        place_point = [0, 0]
+        
+        if step_number == 1:
+            self.get_logger().info("Running Step 1")
+            pick_point, place_point = step_1_instructions(cv_image, self.background)
+            self.place_point1 = place_point
+        elif step_number == 2:
+            if self.place_point1 is None:
+                self.get_logger().warn("Run step 1 before running step 2")
+            else:
+                self.get_logger().info("Running Step 2")
+                pick_point, place_point = step_2_instructions(cv_image, self.background, self.place_point1)
+        elif step_number == 3:
+            self.get_logger().info("Running Step 3")
+            pick_point, place_point = step_3_instructions(cv_image, self.background)
+            self.place_point3 = place_point
+            self.pick_point3 = pick_point
+        elif step_number == 4:
+            if self.place_point3 is None or self.pick_point3 is None:
+                self.get_logger().warn("Run step 3 before running step 4")
+            else:
+                self.get_logger().info("Running Step 4")
+                pick_point, place_point = step_4_instructions(cv_image, self.background, self.pick_point3, self.place_point3)
+                self.place_point4 = place_point
+        elif step_number == 5:
+            self.get_logger().info("Running Step 5")
+            pick_point, place_point = step_5_instructions(cv_image, self.background, self.place_point4)
+
         else:
             self.get_logger().warn(f"Unknown step number {step_number}, returning [0, 0].")
-            image_pick_point = [0, 0]
-            image_place_point = [0, 0]
+            
 
-        response.image_pick_point = image_pick_point
-        response.image_place_point = image_place_point
+        response.image_pick_point = pick_point
+        response.image_place_point = place_point
 
         self.get_logger().info(f"Returned pick and place point for step {step_number}: Pick: [{response.image_pick_point[0]:.3f}, {response.image_pick_point[1]:.3f}], Place [{response.image_place_point[0]:.3f}, {response.image_place_point[1]:.3f}]")
         return response
